34b6c30661f14f9ade0fb955af0e8433bd7d9df1
Jonathan Fieldsend <J.E.Fieldsend@exeter.ac.uk>
2014-10-12 21:05:53 +0100

    Added the ThreadPoolApp

--- Job.java
+++ Job.java
@@ -1,18 +1,19 @@
 public class Job implements Runnable {
     private int jobNumber;
 
-    WorkerThread(int jobNumber) {
+    Job(int jobNumber) {
         this.jobNumber = jobNumber;
     }
 
     public void run() {
-        // Undertake required work, here we will empulate it by simply sleeping for a period
-        System.out.println("Job: " + jobNumber + " is being processed by thread: ", + Thread.currentThread().name);
+        // Undertake required work, here we will emulate it by sleeping for a period
+        System.out.println("Job: " + jobNumber + " is being processed by thread: "
+                  + Thread.currentThread().getName());
         try {
             Thread.sleep((int)(1000));
-            } catch (InterruptedException e) {  
-                // no catching as example should not experience interruptions
-            }
+        } catch (InterruptedException e) {  
+            // no catching as example should not experience interruptions
         }
     }
+
 }
